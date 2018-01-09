aa2baa3cc8f379e36ab59205e0d3acd156ed8d22
Vera Sazonova <verasazonova@gmail.com>
2018-01-09 15:28:11 -0500

    Fixed test

--- compare_models.sh
+++ compare_models.sh
@@ -9,15 +9,18 @@ if [ ! -d logs ]; then
 fi
 
 # Training
+echo "Training"
 for N in 2 5 10 20; do
     echo $N
     python tobe/main.py --model models/train/weights_$N.hdf5 --train -n 50 --filenames resources/processed_corpus_$N.txt --logs logs/log_$N.csv
 done
 
 # Loss, scores for training
+echo "Plotting loss"
 python tobe/plot_results.py -f logs/log_2.csv logs/log_5.csv logs/log_10.csv logs/log_20.csv
 
 # Evaluating
+echo "Evaluating"
 echo "avg,epochs,P,R,F1" > results/results.txt
 for N in 2 5 10 20; do
     echo $N
@@ -26,4 +29,5 @@ for N in 2 5 10 20; do
 done
 
 # Plot context dependence
+echo "Plotting metrics"
 python tobe/plot_results.py -f results/results.txt --metric
